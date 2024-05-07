a13556e7099db632ebe25f8bbc2c40806c62a644
Joao Santos <jv18.victor@gmail.com>
2024-05-07 15:02:31 -0300

    Adiciona storage nessa porra
    Para nao fica toda hora nessa merda voltando ao estado anterior
    Retira o scroll tambem

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ let navBar3 = document.getElementById('nav-list3')
 let navBar4 = document.getElementById('nav-list4')
 let line = document.getElementById('line')
 let redes = document.getElementsByClassName('redes')
-console.log(redes)
+
+window.onload = ()=> {
+    if(localStorage.getItem('dark') === 'true') {
+        checkbox.checked = true
+        document.body.classList.toggle('dark')
+        head.classList.toggle('dark')
+        navBar.classList.toggle('dark')
+        navBar2.classList.toggle('dark')
+        navBar3.classList.toggle('dark')
+        navBar4.classList.toggle('dark')
+        line.classList.toggle('dark')
+        for(let i = 0; i < redes.length; i++) {redes.item(i+1).classList.toggle('dark')}
+    }
+}
 
 checkbox.addEventListener('change', ()=> {
+    localStorage.setItem('dark', checkbox.checked)
     document.body.classList.toggle('dark')
     head.classList.toggle('dark')
     navBar.classList.toggle('dark')
